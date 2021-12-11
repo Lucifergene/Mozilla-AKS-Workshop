7c0f3157c3e342de359ceefe4ac03412ffbab56e
Avik <1828008@kiit.ac.in>
2021-12-11 14:02:21 +0530

    Added Date-Time Functionality

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const http = require('http');
+const date = require('date-and-time');
+
 const port = process.env.PORT || 3000;
+
+const now = new Date();
+const dateString = date.format(now, 'ddd, MMM DD YYYY');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = '<h1>Hello Node!</h1>\n'
+  const msg = '<h1>Hello Everyone!</h1><br><h2>It is ' + dateString + '</h2>';
   res.end(msg);
 });
 
